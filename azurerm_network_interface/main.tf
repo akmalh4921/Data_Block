378a82707be1106004c5e0e35f1f447444c4afab
MD AKMAL HUSSAIN <akmalh4921@gmail.com>
2026-07-17 00:35:25 +0530

    created data.tf in NIC & updated nic main.tf

--- azurerm_network_interface/main.tf
+++ azurerm_network_interface/main.tf
@@ -1,19 +1,3 @@
-data "azurerm_subnet" "data_subnet" {
-  for_each = var.nics
-
-  name                 = each.value.subnet_name
-  virtual_network_name = each.value.virtual_network_name
-  resource_group_name  = each.value.resource_group_name
-}
-
-data "azurerm_public_ip" "data_pip" {
-  for_each = var.nics
-
-  name                = each.value.name
-  resource_group_name = each.value.resource_group_name
-}
-
-
 resource "azurerm_network_interface" "nic" {
   for_each = var.nics
 
